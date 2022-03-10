23a746747ed4b2cb85c2c14ca7cea47b27a4b085
Gustavo Andrade <gustavo_alex3000@hotmail.com>
2022-03-10 12:44:36 -0300

    feat: add header component

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
-
-module.exports = nextConfig
+module.exports = {
+  images: {
+    domains: [
+      "links.papareact.com",
+      "platform-lookadide.fbsbx.com",
+      "firebasestorage.googleapis.com",
+    ],
+  },
+};
